98835aa31577dd1052ffb88d502eef333fcdff9c
Gerardo Arevalo <gerardo@atctech.co>
2016-07-24 23:09:11 -0400

    build shortcut for prod

---
+++ webpack.production.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: __dirname + "/app/main.js",
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.json$/, loader: "json" },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+            { test: /\.css$/, loader: 'style!css?modules!postcss' }
+        ]
+    },
+    postcss: [
+        require('autoprefixer')
+    ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html"
+        }),
+    ],
+}
+
+//very similar to the original Webpack development configuration 
+//with stripped devtool, devServer and HMR configurations.
