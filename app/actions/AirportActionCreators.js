36502e0ff7c34cad8162554fbf278f2da2ef9620
Gerardo Arevalo <gerardo@atctech.co>
2016-07-30 22:33:31 -0400

    updated fetchAirports and fetchTickers for dispatchAsync

--- app/actions/AirportActionCreators.js
+++ app/actions/AirportActionCreators.js
@@ -4,22 +4,11 @@ import AirCheapAPI from '../api/AirCheapAPI';
 
 let AirportActionCreators = {
     //fetch airports
-    fetchAirports() {
-        AirCheapAPI.fetchAirports();
-        AppDispatcher.dispatch({
-            type: constants.FETCH_AIRPORTS,
-        });
-    },
-    fetchAirportsSuccess(response) {
-        AppDispatcher.dispatch({
-            type: constants.FETCH_AIRPORTS_SUCCESS,
-            payload: { response }
-        });
-    },
-    fetchAirportsError(error) {
-        AppDispatcher.dispatch({
-            type: constants.FETCH_AIRPORTS_ERROR,
-            payload: { error }
+    fetchAirports(origin, destination) {
+        AppDispatcher.dispatchAsync(AirCheapAPI.fetchAirports(), {
+            request: constants.FETCH_AIRPORTS,
+            success: constants.FETCH_AIRPORTS_SUCCESS,
+            failure: constants.FETCH_AIRPORTS_ERROR
         });
     },
 
@@ -33,22 +22,11 @@ let AirportActionCreators = {
     },
 
     //fetch tickets
-    fetchTickets() {
-        AirCheapAPI.fetchTickets();
-        AppDispatcher.dispatch({
-            type: constants.FETCH_TICKETS,
-        });
-    },
-    fetchTicketsSuccess(response) {
-        AppDispatcher.dispatch({
-            type: constants.FETCH_TICKETS_SUCCESS,
-            payload: { response }
-        });
-    },
-    fetchTicketsError(error) {
-        AppDispatcher.dispatch({
-            type: constants.FETCH_TICKETS_ERROR,
-            payload: { error }
+    fetchTickets(origin, destination) {
+        AppDispatcher.dispatchAsync(AirCheapAPI.fetchTickets(origin, destination), {
+            request: constants.FETCH_TICKETS,
+            success: constants.FETCH_TICKETS_SUCCESS,
+            failure: constants.FETCH_TICKETS_ERROR
         });
     }
 
