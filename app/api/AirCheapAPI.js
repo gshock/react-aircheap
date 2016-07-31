79aa77d2aece102d2f35f8d7b19a88b78aa5599e
Gerardo Arevalo <gerardo@atctech.co>
2016-07-30 22:37:41 -0400

    updated API returning the promise created by the fetch operation and chaining to the JSON parsing operation

--- app/api/AirCheapAPI.js
+++ app/api/AirCheapAPI.js
@@ -3,27 +3,12 @@ import AirportActionCreators from '../actions/AirportActionCreators';
 
 let AirCheapAPI = {
     fetchAirports() {
-        fetch('airports.json')
-            .then((response) => response.json())
-            .then((responseData) => {
-                // Call the AirportActionCreators success action with the parsed data
-                AirportActionCreators.fetchAirportsSuccess(responseData);
-            })
-            .catch((error) => {
-                // Call the AirportActionCreators error action with the error object
-                AirportActionCreators.fetchAirportsError(error);
-            });
+        return fetch('airports.json')
+            .then((response) => response.json());
     },
-
     fetchTickets(origin, destination) {
-        fetch('flights.json')
-            .then((response) => response.json())
-            .then((responseData) => {
-                AirportActionCreators.fetchTicketsSuccess(responseData);
-            })
-            .catch((error) => {
-                AirportActionCreators.fetchTicketsError(error);
-            });
+        return fetch('flights.json')
+            .then((response) => response.json());
     }
 
 };
